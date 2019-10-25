f9c7b0c39df45a6a1363393f1786d73d042ad81c
Pete331 <53825841+Pete331@users.noreply.github.com>
2019-10-25 16:39:50 +0800

    Updates

---
+++ precache-manifest.5823b3dab3999718235cf47c9b85c1b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74aada61b255eaef0b79b973efcbdfa6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f8708f55550fd93d0041",
+    "url": "/robofriends/static/css/main.8517169c.chunk.css"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/js/2.3cd86ebb.chunk.js"
+  },
+  {
+    "revision": "f8708f55550fd93d0041",
+    "url": "/robofriends/static/js/main.45268409.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
